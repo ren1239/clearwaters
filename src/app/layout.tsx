36c43ddf7a2596dc471e4e72a73633a069b17188
rentan-mbp <tan.re60@gmail.com>
2025-07-25 11:01:42 +0800

    lp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Clear Waters Capital",
+  title: "Clear Waters Capital LP",
   description: "Clear Minded Asset Management",
   icons: {
     icon: [
